123137dcff33546245ee485909fba9caed44d69d
Randy Cisneros <rlcisneros90@gmail.com>
2023-04-23 14:30:41 -0600

    Add: Functionality to the DOM to update/cear form, book trip, and display cards.

--- src/scripts.js
+++ src/scripts.js
@@ -9,26 +9,30 @@ import './images/turing-logo.png'
 import Trips from '../src/all-trips'
 import Destinations from './all-destinations';
 import Traveler from './all-travelers';
+// import singleTraveler from './single-traveler';
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
 //Query Selectors
 const form = document.querySelector(".travel-form")
-const mediaElement = document.querySelector(".media-element")
+const acceptedMediaElement = document.querySelector(".accepted-element")
+const pendingMediaElement = document.querySelector(".pending-element")
 const travelerName = document.querySelector(".main-title")
 const totalTrips = document.querySelector(".total-trips")
 const amountTrips = document.querySelector(".amount-trips")
-
+const formDestinations = document.querySelector("#destination")
+const formDuration = document.querySelector("#duration")
+const formTravelers = document.querySelector("#travelers")
+const formEstimation = document.querySelector("#cost")
+const bookTrip = document.querySelector(".book-trip")
+const formDate = document.querySelector("#date")
 
 //Global Variables
-let allTrips, allDestinations, travelers
+let allTrips, allDestinations, allTravelers
 
 //Event Listeners
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    displayCalculatedCost();
-  });
+
 // Functions
 window.addEventListener('load', () => {
   Promise.all(apiCalls)
@@ -38,55 +42,144 @@ window.addEventListener('load', () => {
       const trips = apiCallsArray[2].trips
       const destinations = apiCallsArray[3].destinations
       displayTripCardsApproved(trips, destinations, traveler)
+      displayTripCardsPending(trips, destinations, traveler)
       displayTravelerName(traveler);
       displayTotalTrips(traveler, trips)
       displayAmountTrips(traveler, trips, destinations)
+      displayTripsForForm(destinations)
+      updateDateCalendar()
     })
     .catch(error => console.log(error, "error"))
 })
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  Promise.all(apiCalls)
+    .then((apiCallsArray) => {
+      const destinations = apiCallsArray[3].destinations
+      displayCalculatedCost(destinations);
+    })
+    .catch(error => console.log(error, "error"))
+});
+
+bookTrip.addEventListener("click", function(event) {
+  event.preventDefault()
+  Promise.all(apiCalls)
+    .then((apiCallsArray) => {
+      const travelers = apiCallsArray[0].travelers
+      const trips = apiCallsArray[2].trips
+      const destinations = apiCallsArray[3].destinations
+      submitTrip(trips, destinations, travelers)
+    })
+    .catch(error => console.log(error, "error"))
+})
+
+function submitTrip(trips, destinations, traveler) {
+  var date = (document.querySelector("#date").value).split('-').join('/')
+  var duration = parseFloat(document.querySelector("#duration").value);
+  var inputTravelers = parseFloat(document.querySelector("#travelers").value);
+  var destination = parseFloat(document.querySelector("#destination").value);
+
+  if (!formDate.value || !formDestinations.value || !formTravelers.value || !formDuration.value) {
+    window.alert("At least one of the required values are missing or invalid, try again please!")
+  } else {
+    fetch("http://localhost:3001/api/v1/trips", {
+    method: "POST",
+    body: JSON.stringify({
+    id: Number(Date.now()),
+    userID: traveler[2].id,
+    destinationID: destination,
+    travelers: inputTravelers,
+    date: date,
+    duration: duration,
+    status: 'pending',
+    suggestedActivities: []
+  }),
+  headers: {
+    "Content-Type": "application/json"
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+  allTrips.addTrip(data.newTrip)
+  displayTripCardsPending(trips, destinations, traveler)
+  displayTotalTrips(traveler, trips)
+  displayAmountTrips(traveler, trips, destinations)
+  displayCalculatedCost(destinations)
+  resetForm()
+  })
+  .catch(error => console.log(error))
+  }
+};
+
+function updateDateCalendar() {
+  let today = new Date().toLocaleDateString('en-CA')
+  formDate.setAttribute("min", today)
+};
+
+function resetForm() {
+  formDestinations.value = ""
+  formDate.value = ""
+  formDuration.value = ""
+  formTravelers.value = ""
+  formEstimation.value = ""
+};
+
+const displayCalculatedCost = (destinations) => {
+  allDestinations = new Destinations(destinations)
+  const totalCost = allDestinations.getDestinationCost(Number(formDestinations.value), Number(formTravelers.value), Number(formDuration.value))
+  formEstimation.value = "$" + totalCost.toFixed(2);
+  return false 
+};
+
+const displayTripsForForm = (destinations) => {
+  formDestinations.innerHTML =''
+  destinations.forEach((destination) => {
+    formDestinations.innerHTML += `<option id="${destination.id}" value="${destination.id}">${destination.destination}</option>`
+   })
+};
 
 const displayAmountTrips = (traveler, trips, destinations) => {
   allTrips = new Trips(trips)
   destinations = new Destinations(destinations)
   amountTrips.innerHTML = `You spent a total of $${parseFloat(allTrips.calculateTripCost(traveler[2], destinations)).toLocaleString()} on trips.`
-}
+};
 
 const displayTotalTrips = (traveler, trips) => {
   allTrips = new Trips(trips)
   totalTrips.innerHTML = `You went on ${allTrips.getTotalTrips(traveler[2])} total trips!`
-}
+};
 
 const displayTravelerName = (traveler) => {
   travelerName.innerHTML = `Welcome back to<br>Odyssey Vacations,<br>${traveler[2].name}!</h1>`
-}
+};
 
 const displayTripCardsApproved = (trips, destinations, traveler) => {
   allTrips = new Trips(trips)
   allDestinations = new Destinations(destinations)
-  travelers = new Traveler(traveler)
+  // allTravelers = new Travelers(traveler)
     const travelerTrips = allTrips.getTripsByTravelerId(traveler[2]).filter((trip) => trip.status === 'approved')
     travelerTrips.forEach((trip) => {
     const travelerDest = allDestinations.getDestinationById(trip.destinationID)
-    const cardElement = document.createElement('div');
-    cardElement.classList.add('card');
-
-    const destinationImage = `<img src="${travelerDest.image}" class="card-image" alt="${travelerDest.alt}">`;
-    const destinationTitle = `<span class="card-info">${travelerDest.destination}<br>Date: ${trip.date}<br>Duration: ${trip.duration}<br>Travelers: ${trip.travelers}</span>`;
-    const cardContent = `${destinationImage}${destinationTitle}`;
-    cardElement.innerHTML = cardContent;
-    mediaElement.appendChild(cardElement);
+    acceptedMediaElement.innerHTML += `<div class="card" tabindex="0">
+                                <img src="${travelerDest.image}" class="card-image" alt="${travelerDest.alt}">
+                                <span class="card-info">${travelerDest.destination}<br>Date: ${trip.date}<br>Duration: ${trip.duration}<br>Travelers: ${trip.travelers}</span>
+                              </div>`
   });
 };
 
-function displayCalculatedCost() {
-    var date = new Date(document.querySelector("#date").value);
-    var duration = document.querySelector("#duration").value;
-    console.log(duration)
-    var travelers = document.querySelector("#travelers").value;
-    var destination = document.querySelector("#destination").value;
-    var costPerDay = 100;
-    var costPerPerson = costPerDay * duration;
-    var totalCost = costPerPerson * travelers;
-    document.querySelector("#cost").value = "$" + totalCost.toFixed(2);
-    return false
-}
+const displayTripCardsPending = (trips, destinations, traveler) => {
+  allTrips = new Trips(trips)
+  allDestinations = new Destinations(destinations)
+  // allTravelers = new Travelers(traveler)
+  pendingMediaElement.innerHTML = ''
+    const travelerTrips = allTrips.getTripsByTravelerId(traveler[2]).filter((trip) => trip.status === 'pending')
+    travelerTrips.forEach((trip) => {
+    const travelerDest = allDestinations.getDestinationById(trip.destinationID)
+    pendingMediaElement.innerHTML += `<div class="card" tabindex="0">
+                                <img src="${travelerDest.image}" class="card-image" alt="${travelerDest.alt}">
+                                <span class="card-info">${travelerDest.destination}<br>Date: ${trip.date}<br>Duration: ${trip.duration}<br>Travelers: ${trip.travelers}</span>
+                              </div>`
+  });
+};
+
